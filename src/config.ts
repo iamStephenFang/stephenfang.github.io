32f903c04a1d04d11a80580e3011359100bcef68
StephenFang <fangxuanmiao@hotmail.com>
2023-12-18 23:47:08 +0800

    fix: base color

--- src/config.ts
+++ src/config.ts
@@ -22,31 +22,31 @@ export const SOCIALS: SocialObjects = [
   {
     name: "Github",
     href: "https://github.com/iamstephenfang",
-    linkTitle: ` ${SITE.title} on Github`,
+    linkTitle: ` ${SITE.author} on Github`,
     active: true,
   },
   {
     name: "Instagram",
     href: "https://instagram.com/iamstephenfang",
-    linkTitle: `${SITE.title} on Instagram`,
+    linkTitle: `${SITE.author} on Instagram`,
     active: true,
   },
   {
     name: "Mail",
     href: "mailto:fangxuanmiao@hotmail.com",
-    linkTitle: `Send an email to ${SITE.title}`,
+    linkTitle: `Send an email to ${SITE.author}`,
     active: true,
   },
   {
     name: "Twitter",
     href: "https://twitter.com/fangxuanmiao",
-    linkTitle: `${SITE.title} on Twitter`,
+    linkTitle: `${SITE.author} on Twitter`,
     active: true,
   },
   {
     name: "Telegram",
     href: "https://t.me/stephenfang",
-    linkTitle: `${SITE.title} on Telegram`,
+    linkTitle: `${SITE.author} on Telegram`,
     active: true,
   },
 ];
