a2a7dcdba09fd97c2f16744ae47b212e565feef0
iamStephenFang <Fangxuanmiao@hotmail.com>
2025-03-13 00:49:57 +0800

    fix: astro check issues

--- src/config.ts
+++ src/config.ts
@@ -4,11 +4,17 @@ export const SITE = {
   profile: "https://jike.city/stephenfang",
   desc: "stephenfang.me",
   title: "StephenFang's Blog",
+  ogImage: "https://image.stephenfang.me/og-image.jpeg",
   lightAndDarkMode: true,
   postPerIndex: 4,
   postPerPage: 4,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
   showBackButton: true,
+  editPost: {
+    url: "https://github.com/iamStephenFang/stephenfang.github.io/edit/main/src/content/blog",
+    text: "Suggest Changes",
+    appendFilePath: true,
+  },
   dynamicOgImage: true,
 };
